0cd0de8f243fff4ef30931a87cc5e4caa567e145
benebsiny <stu995106@hotmail.com.tw>
2020-01-15 00:47:02 +0800

    完成2008~2020總統大選的顏色地圖
    Former-commit-id: 3902f74d2b2f5c891c74ae1688a59009ee4afa60 [formerly c60d6b6fab134b5a530ab59e4ad6cfa740fd9044]
    Former-commit-id: fd6220e6e4b3244589f4bbbe32f0a6e2f1be8124

---
+++ README.md
@@ -0,0 +1,19 @@
+# 苑裡鎮各里投票地圖
+## 內容
+內容包含2008~2020年總統大選，苑裡鎮各里的投票比率顏色圖。裡面的顏色，票多的深，票少的淺。
+
+## DEMO
+可以直接從這裡看結果 [DEMO](https://benebsiny.github.io/Election-result-graph-for-Yuanli-township)
+
+或者clone下來，所需步驟：
+1. 安裝環境
+```SHELL
+npm i
+```
+2. 運行環境
+```SHELL
+npm start
+```
+3. 打開頁面 `127.0.0.1:3000`
+
+PS. 若直接打開html檔，會被瀏覽器檔掉哦
--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-const geojson2svg = require('geojson2svg')
-const parseSVG = require('parse-svg')
-const fs = require('fs')
-const path = require('path')
+const Koa = require('koa');
+const static = require('koa-static');
+const path = require('path');
+const { createReadStream } = require('fs');
 
+const app = new Koa();
 
-const jsonfile = path.join(__dirname, '全台灣村里界圖_20140501.json')
-
-fs.readFile(jsonfile, { encoding: 'utf-8' }, (err, data) => {
-    if (!err) {
-        conv(data);
-    }
+app.use(static(path.join(`${__dirname}`)))
+app.use(async (ctx) => {
+    ctx.type = 'html';
+    ctx.body = createReadStream('index.html');
 })
 
-function conv(geojsonContent) {
-    // console.log(geojsonContent);
-    
-    const converter = geojson2svg({})
-
-    const svgStrings = converter.convert(geojsonContent, {})
-    console.log(svgStrings); // undefined
-    
-    // const svgElement = svgStrings.map(function (svgString) {
-    //     return parseSVG(svgString)
-    // })
-}
-    
-
-
+app.listen(3000, '127.0.0.1', () => {
+    console.log('http://127.0.0.1:3000');  
+})
